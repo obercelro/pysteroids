ea70347d5855d028d6bfa1b24d77ac6c34335808
obercelro <robmcelroy145@gmail.com>
2025-11-08 23:31:16 -0600

    feat: player drawn on screen, basic movement complete

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
     while pygame.get_init():
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, color = "black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
-
-
+        clock.tick(60)
+        dt = clock.get_time() / 1000
+        
 if __name__ == "__main__":
     main()
